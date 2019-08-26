7ad26711050ab8f7c3aef04ace57fa12ae571656
Justas <16074672+stromjust@users.noreply.github.com>
2019-08-27 00:22:06 +0300

    Added algorithm to process live message feed

--- content.js
+++ content.js
@@ -1,13 +1,28 @@
-
 const selector = 'div[role="log"]';
-
-loadChat(selector, function(chat) {
-	console.log(chat);
-})
+loadChat(selector, processMessages);
 
 
+function processMessages(chat) {
+	let lastMessage = document.createElement('div');
+	setInterval(function() {
+		try {
+			const messageArray = Array.from(chat.childNodes);
+			const length = messageArray.length - 2;
+			let   lastMessageIndex = messageArray.indexOf(lastMessage);
 
-
+			if(length > lastMessageIndex) {
+				if(lastMessageIndex == -1) lastMessageIndex = 0;
+				for(let i = lastMessageIndex + 1; i <= length; i++) {
+					console.log(messageArray[i].innerText);
+				}
+			}
+			lastMessage = messageArray[length];
+		}
+		catch(err) {
+			// ignore
+		}
+	}, 2000);
+}
 
 function loadChat(selector, callback) {
 	window.setTimeout(function() {
